fbc402a9c3d2a7d7960f669cd106710d4dc45956
jordanLindo <jordanlindo@live.com>
2022-08-02 22:38:03 -0500

    Worked in server, Formatted data, Adjusted linter to preference

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 'use strict';
 
 const { request } = require('express');
@@ -20,38 +22,76 @@ app.use(cors());
 const PORT = process.env.PORT || 3002;
 
 
-app.get('/',(request, response) => {
+app.get('/', (request, response) => {
     response.send('Active');
 });
 
-/* //add a pizza route
-app.get('/pizza', (request, response) => {
-    try{
-      let pizzaType = request.query.pizzatype;
-      // http://localhost:3001/pizza?pizzatype=Chicago%20Pizza
-      //add data file and look at find(will find the first and returns only that match)
-      // let dataToSend = data.find(pizza => pizza.pizzatype === pizzaType);
-      let dataToInstant = data.find(pizza => pizza.pizzatype === pizzaType);
-      let dataToSend = new Pizza(dataToInstant);
-      response.send(dataToSend);
-      // now create a class below
-    } catch(error){
-      //create a new instance of error.
-      next(error);
-      // this will instantiate any new error
+app.get('/weather', (request, response) => {
+    try {
+        console.log('In weather try');
+        let searchQuery = request.query.searchQuery;
+        let lat = request.query.lat;
+        let lon = request.query.lon;
+        let aCity = data.find(city => city.city_name === searchQuery || city.lat === lat && city.lon === lon);
+        console.log(searchQuery);
+        console.log(lat);
+        console.log(lon);
+        if(aCity !== undefined){
+            let cityInstance = new City(aCity);
+            let dataToSend = cityInstance.data.map((val) => {
+                return (new Forecast(val.valid_date,'Low of '+val.high_temp+', high of '+val.low_temp+' with '+val.weather.description));
+            });
+            response.send(dataToSend);
+        }
+    }catch(error){
+        console.log(error);
     }
-  }); */
+});
+
+app.get('/weather', (request, response) => {
+    try {
+        console.log('In weather try');
+        let lat = request.query.lat;
+        let lon = request.query.lon;
+        let aCity = data.find(city => city.city_name === searchQuery);
+        let cityInstance = new City(aCity);
+        let dataToSend = cityInstance.data.map((val) => {
+            return (new Forecast(val.valid_date,'Low of '+val.high_temp+', high of '+val.low_temp+' with '+val.weather.description));
+        });
+        response.send(dataToSend);
+    }catch(error){
+        console.log(error);
+    }
+});
 
 //catch-all
-app.get('*',(request,response) => {
-  response.send('The route was not found. Error 404');
+app.get('*', (request, response) => {
+    response.send('The route was not found. Error 404');
 });
 
 //Classes
+class City{
+    constructor(cityObject){
+        this.data = cityObject.data;
+        this.city_name = cityObject.city_name;
+        this.lon = cityObject.lon;
+        this.lat = cityObject.lat;
+        this.timezone = cityObject.timezone;
+        this.country_code = cityObject.country_code;
+        this.state_code = cityObject.state_code;
+    }
+}
+
+class Forecast{
+    constructor(date, description){
+        this.date = date;
+        this.description = description;
+    }
+}
 
 //Errors
-app.use((error, request, response, next) =>{
-  response.status(500).send(error.message);
+app.use((error, request, response, next) => {
+    response.status(500).send(error.message);
 });
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
