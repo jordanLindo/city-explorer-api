8f6ed3cb2cb634bc817a8e283b39d18b0b865dbb
jordanLindo <jordanlindo@live.com>
2022-08-06 17:27:48 -0500

    Update error handling

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const express = require('express');
 const cors = require('cors');
 const weather = require('./modules/weather.js');
 const movie = require('./modules/movie.js');
-const { application } = require('express');
 
 const app = express();
 app.use(cors());
@@ -14,20 +13,33 @@ const PORT = process.env.PORT || 3002;
 app.get('/weather', weatherHandler);
 app.get('/movie',movieHandler);
 
-function weatherHandler(request, response) {
-    const { lat, lon } = request.query;
-    weather(lat, lon).then(summaries => response.send(summaries)).catch((error) => {
-        console.error(error);
-        response.status(200).send('Sorry. Something went wrong!');
-    });
+function weatherHandler(request, response, next) {
+    try {
+        const { lat, lon } = request.query;
+        weather(lat, lon).then(summaries => response.send(summaries)).catch((error) => {
+            console.error(error);
+            response.status(200).send('Sorry. Something went wrong!');
+        });
+    } catch (error) {
+        next(error);
+    }
 }
 
-function movieHandler(request,response){
-    const keyword = request.query;
-    movie(keyword).then(summaries => response.send(summaries)).catch((error) => {
-        console.error(error);
-        response.status(200).send('Sorry. Something went wrong!');
-    });
+function movieHandler(request,response, next){
+    try {
+        const keyword = request.query;
+        movie(keyword).then(summaries => response.send(summaries)).catch((error) => {
+            console.error(error);
+            response.status(200).send('Sorry. Something went wrong!');
+        });
+    } catch (error) {
+        next(error);
+    }
 }
+
+//Errors
+app.use((error, request, response, next) => {
+    response.status(500).send(error.message);
+});
 
 app.listen(PORT, () => console.log(`Server up on ${PORT}`));
